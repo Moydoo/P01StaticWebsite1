1dfa8ca652169d91cc6eaed1351ea65e7d0cbccb
Michal <m.stefanski98@gmail.com>
2023-05-17 14:25:20 +0200

    changes

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const gallery = document.getElementById('gallery');
+
+window.onmousemove = e => {
+    const mouseX = e.clientX,
+        mouseY = e.clientY;
+
+    const xDecimal = mouseX / window.innerWidth,
+        yDecimal = mouseY / window.innerHeight;
+
+    const maxX = gallery.offsetWidth - window.innerWidth,
+        maxY = gallery.offsetHeight - window.innerHeight;
+
+    const panX = maxX * xDecimal * -1,
+    panY = maxY * yDecimal * -1;
+
+    gallery.animate( {
+        transform: `translate(${panX}px, ${panY}px)`
+    }, {
+        duration: 5000,
+        fill: "forwards",
+        easing: "ease"
+    })
+}
+
+console.log(window.innerHeight, window.innerWidth, gallery.offsetHeight, gallery.offsetWidth);
